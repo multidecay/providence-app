0c7a3c76b90df6ba50c638ce03ece2bda2cede18
Minako Norabell <minako@keemail.me>
2023-09-28 15:19:21 +0700

    feature: new navbar (#5)

--- resources/views/pages/dashboard/devices.blade.php
+++ resources/views/pages/dashboard/devices.blade.php
@@ -22,7 +22,7 @@
                     <td>{!! $device->maid()->get()->first()->name !!}</td>
                     <td>{!! $device->notes !!}</td>
                     <td>
-                        <a href="/dashboard/device/{!! $device->id !!}" target="_blank">
+                        <a href="/dashboard/device/{!! $device->id !!}">
                             <button class="p-2 my-2 bg-black text-white rounded font-semibold">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/><polyline points="15 3 21 3 21 9"/><line x1="10" y1="14" x2="21" y2="3"/></svg>
                             </button>
