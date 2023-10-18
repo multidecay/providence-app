9a41e30bef04f7521147e39627a3d66352252b8b
Minako Norabell <minako@keemail.me>
2023-10-18 11:11:34 +0700

    docs: update README.md

---
+++ database/seeders/CodegenSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Codegen;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CodegenSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+    }
+}
